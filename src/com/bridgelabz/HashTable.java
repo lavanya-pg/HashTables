6034e5fdca4f1ad18dcdede535d170e9b1f878b9
lavanya-pg <lavanyapg8@gmail.com>
2021-07-06 18:45:12 +0530

    ADDED:Code for Finding Frequency for a Paragraph

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -5,7 +5,11 @@ public class HashTable
 	public static void main(String[] args) 
 	{
         HashTableImpl<String, Integer> hash = new HashTableImpl();
-        String message = "To be or not to be";
+        String message = "“Paranoids are not\r\n"
+        		+ "paranoid because they are paranoid but\r\n"
+        		+ "because they keep putting themselves\r\n"
+        		+ "deliberately into paranoid avoidable\r\n"
+        		+ "situations”";
 
         //String to String[] conversion.
         String[] messageArray = message.toLowerCase().split(" ");
