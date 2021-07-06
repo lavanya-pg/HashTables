fa3d3d29f410299775aae5b2179b6a77f286acda
lavanya-pg <lavanyapg8@gmail.com>
2021-07-06 18:36:17 +0530

    ADDED:Code to calculate the frequency of the words

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -2,10 +2,26 @@ package com.bridgelabz;
 
 public class HashTable 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		System.out.println("Welcome to the Hash Table Program");
+        HashTableImpl<String, Integer> hash = new HashTableImpl();
+        String message = "To be or not to be";
 
-	}
+        //String to String[] conversion.
+        String[] messageArray = message.toLowerCase().split(" ");
+
+        //Iterate within the array
+        for(String word: messageArray) {
+            //Search for the word in the linked list
+            Integer value =  hash.get(word);
+            if( value == null)
+                value = 1;
+            else
+                value = value + 1;
+            hash.add(word , value);
+        }
+        //Display the linked list
+        System.out.println(hash);
+    }
 
 }
